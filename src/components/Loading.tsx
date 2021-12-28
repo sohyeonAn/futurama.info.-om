8aa0971a2b569c22cb679cf8220322db77c9fcdd
sohyeonAn <ansh4393@gmail.com>
2021-12-28 13:23:07 +0900

    💄 로딩 페이지 수정

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,11 +2,33 @@ import styled from "@emotion/styled"
 
 export const Loading = () => {
   return (
-    <div>Loading...</div>
+    <LoadingBox>
+      <img
+        src="/images/loading.png"
+        alt="Loading"
+      />
+      <p>Loading...</p>
+    </LoadingBox>
   )
 }
 
 
-// const LoadingBox = styled.div`
-//   text-align: center;
-// `
+const LoadingBox = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  img {
+    width: 15em;
+    height: 15em;
+  }
+
+  p {
+    margin: 0;
+    font-size: 3em;
+    font-weight: bold;
+  }
+`
